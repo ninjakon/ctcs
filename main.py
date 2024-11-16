02069634e5fcb5e351e0246ec6393760a98701b7
Arno Breitfuss <arno.breitfuss@gmx.at>
2024-11-16 15:05:06 +0100

    add stuff to make it easier to run on other machines

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ def main():
     parser = argparse.ArgumentParser("Model Test Runner")
     parser.add_argument("--device", help="The device to run HuggingFace models on.", type=str)
     args = parser.parse_args()
+    device = args.device
+
+    print("Welcome to the model test runner!")
+    if device is None:
+        print("> You did not specify a device to run HuggingFace models on.")
+        print("> So by default the pipeline will use CPU.")
+    else:
+        print(f"> The device to run HuggingFace models on is '{device}' as specified.")
 
     prompt = "Who is the president of the USA?"
     test_runner = TestRunner(device=args.device)
